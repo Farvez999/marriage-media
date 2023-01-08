044f350cee38284c210892ea990d8f9b39ff738b
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-01-08 12:11:48 +0600

    Footer Design

--- client/src/Layout/Main.js
+++ client/src/Layout/Main.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Home from "../Pages/Home/Home";
+import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import NavHome from "../Pages/Shared/Navbar/NavHome";
 
@@ -9,7 +10,7 @@ const Main = () => {
             <NavHome></NavHome>
             {/* <Navbar></Navbar> */}
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
