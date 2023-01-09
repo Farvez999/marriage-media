61159edbe48fd68ba8503f0ba2723d56a36cf92f
Abu Saleh Sami <65762424+salehsamir@users.noreply.github.com>
2023-01-09 17:30:23 +0600

    about-page

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="about">
-        <div className="left">
+    <div className="about grid grid-cols-3 gap-4">
+        <div className="left col-span-2">
         <div className="hero min-h-screen bg-base-200">
       <div className="hero-content  text-left">
         <div>
@@ -23,23 +23,8 @@ function About() {
             a id nisi.
           </p>
         </div>
-        {/* <div>
-          <h1 className="text-2xl font-bold">About Us</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div> */}
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-2xl font-bold">About Us</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        
       
-    </div>
-  </div>
-</div>
       </div>
       
     </div>
@@ -48,6 +33,15 @@ function About() {
         </div>
 
         <div className="right">
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md text-left">
+      <h1 className="text-2xl font-bold">About Us</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      
+    </div>
+  </div>
+</div>
 
         </div>
           
